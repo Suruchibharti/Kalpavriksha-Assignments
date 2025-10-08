1abcd2f3567ab2f11c4ef47a7d98c6d214fe4267
Suruchi Bharti <116417694+Suruchibharti@users.noreply.github.com>
2025-10-08 09:19:26 +0530

    Update Calculator.c
    Resolved bugs and updated Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -3,16 +3,18 @@
 
 #define SIZE 900   
 
-//use to chek operator
+//use to check operator
 int operatorCheck(char c) {
     return (c=='+' || c=='-' || c=='*' || c=='/');
 }
 
 //use for priority check of operator
 int precedence(char c) {
-    if (c=='*' || c=='/') return 2;
-    if (c=='+' || c=='-') return 1;
-    return 0;
+    if (c == '/') return 3;   // Changed precedence for strict DMAS
+    if (c == '*') return 2;
+    if (c == '+') return 1;
+    if (c == '-') return 0;
+    return -1;
 }
 
 //use for evaluate the operation b/w two operand
@@ -34,74 +36,81 @@ int is_space(char c) {
     return (c == ' ');
 }
 
-//use to chek digit
+//use to check digit
 int is_digit(char c) {
     return (c >= '0' && c <= '9');
 }
 
+// merge numbers with spaces like "2 2" → 22
+int parse_number(char *exp, int *i, int len) {
+    int val = 0;
+    while (*i < len) {
+        if (is_space(exp[*i])) {
+            (*i)++; // skip space between digits
+            continue;
+        }
+        if (!is_digit(exp[*i])) break;
+        val = val * 10 + (exp[*i] - '0');
+        (*i)++;
+    }
+    return val;
+}
+
 //it evaluates the expression
-//we are using two stacks: numStacks for number and opStack for operator
-//nTop : index of top number in the numStacks 
-//opTop : index of top operator in the opStack
 int calc(char *exp, int *err) {
     int numStack[SIZE], nTop=-1;     
     char opStack[SIZE]; int oTop=-1;
-    int len=strlen(exp);
+    int len = strlen(exp);
 
-    for (int i=0;i<len;) {
+    for (int i = 0; i < len;) {
         if (is_space(exp[i])) { i++; continue; }
 
         if (is_digit(exp[i])) {
-            int val=0;
-            while (i<len && is_digit(exp[i])) {
-                val=val*10+(exp[i]-'0');
-                i++;
-            }
-            numStack[++nTop]=val;
+           
+            int val = parse_number(exp, &i, len);
+            numStack[++nTop] = val;
             continue;
         }
 
         if (operatorCheck(exp[i])) {
-            while (oTop>=0 && precedence(opStack[oTop]) >= precedence(exp[i])) {
-                int b=numStack[nTop];
-                nTop--;
-                int a=numStack[nTop];
-                nTop--;
-                char op=opStack[oTop];
-                oTop--;
-                nTop++;
-                numStack[nTop]=calculateTheOperations(a,b,op,err);
+            while (oTop >= 0 && precedence(opStack[oTop]) > precedence(exp[i])) {
+                int b = numStack[nTop--];
+                int a = numStack[nTop--];
+                char op = opStack[oTop--];
+                numStack[++nTop] = calculateTheOperations(a, b, op, err);
                 if (*err) return 0;
             }
-            ++oTop;
-            opStack[oTop]=exp[i];
+            opStack[++oTop] = exp[i];
         } else {
-            *err=2; return 0;
+            *err = 2; return 0;
         }
         i++;
     }
 
-    while (oTop>=0) {
-        int b=numStack[nTop--];
-        int a=numStack[nTop--];
-        char op=opStack[oTop--];
-        numStack[++nTop]=calculateTheOperations(a,b,op,err);
+    // Remaining operations
+    while (oTop >= 0) {
+        int b = numStack[nTop--];
+        int a = numStack[nTop--];
+        char op = opStack[oTop--];
+        numStack[++nTop] = calculateTheOperations(a, b, op, err);
         if (*err) return 0;
     }
+
     return numStack[nTop];
 }
 
 //this is the main function
 int main() {
     char expression[SIZE];
-       printf("Enter the expression: ");
-     scanf("%[^\n]", expression); 
-     int err=0;
-   int output=calc(expression,&err);
+    printf("Enter the expression: ");
+    scanf(" %[^\n]", expression);  // fixes space reading
+
+    int err=0;
+    int output=calc(expression, &err);
 
     if (err==1) printf("Error: Division by zero.\n");
     else if (err==2) printf("Error: Invalid expression.\n");
     else printf("Output = %d\n", output);
 
-    return 0;
+return 0;
 }
